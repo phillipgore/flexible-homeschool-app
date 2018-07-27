313a95dd97bf6c1b88228beffab6166f8ab0f0a5
phillipgore <phillipagore@me.com>
2018-07-27 14:45:03 -0500

    mongodb indexing

--- imports/api/lessons/lessons.js
+++ imports/api/lessons/lessons.js
@@ -4,7 +4,7 @@ import SimpleSchema from 'simpl-schema';
 export const Lessons = new Mongo.Collection('lessons');
 
 if ( Meteor.isServer ) {
-    Lessons.rawCollection().createIndex({ subjectId: 1, weekId: 1, order: 1 }, {name: "lessonsIndex"});
+    Lessons.rawCollection().createIndex({ weekId: 1, subjectId: 1, order: 1 }, {name: "lessonsIndex"});
 }
 
 Lessons.allow({
