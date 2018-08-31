beb532331b8b4753f8a6817864cea7694a16fb83
phillipgore <phillipagore@me.com>
2018-08-31 15:35:46 -0500

    back office: in progress

--- imports/api/office/server/publications.js
+++ imports/api/office/server/publications.js
@@ -1,7 +1,7 @@
 Meteor.publish('allAccounts', function (){ 
-	if (!this.userId) {
+	if (!this.userId || Meteor.users.findOne({ _id: this.userId }).info.role != 'Application Administrator') {
 		return this.ready();
 	}
 
-	return Meteor.users.find({'info.role': 'Administrator'}, {fields: {'info.firstName': 1, 'info.lastName': 1, 'info.relationshipToStudents': 1}});
+	return Meteor.users.find({'info.role': 'Administrator'}, {fields: {'info.firstName': 1, 'info.lastName': 1, 'info.relationshipToStudents': 1, 'info.role': 1}});
 });
