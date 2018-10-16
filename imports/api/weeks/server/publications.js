4cc405867f3e5aebd2e5b797911c020fc0512c7c
phillipgore <phillipagore@me.com>
2018-10-16 05:57:08 -0500

    reports refactor

--- imports/api/weeks/server/publications.js
+++ imports/api/weeks/server/publications.js
@@ -20,40 +20,41 @@ Meteor.publish('weeksPath', function(termId, studentId, showAllWeeks) {
 			return this.ready();
 		}
 
-		SchoolYears.find();
-		Terms.find();
-		Lessons.find();
-
 		let self = this;
 
-		let groupId = Meteor.users.findOne({_id: this.userId}).info.groupId;
-		let weeks = Weeks.find({termId: termId, groupId: groupId, deletedOn: { $exists: false }}, {sort: {order: 1}, fields: {groupId: 0, userId: 0, createdOn: 0, deletedOn: 0}});
-		let totalWeekStatus = []
-
-		weeks.map((week) => {
-			week = weekStatus(week, week._id, studentId);
-			totalWeekStatus.push(week.status)
-			self.added('weeks', week._id, week);
-		});
-		
-		function getStatus(status) {
-			if (_.includes(status, 'partial')) {
-				return'partial';
-			}
-			if (_.includes(status, 'completed') && !_.includes(status, 'empty') && !_.includes(status, 'pending')) {
-				return 'completed';
-			}
-			if (_.includes(status, 'empty') && !_.includes(status, 'completed') && !_.includes(status, 'pending')) {
-				return 'empty';
-			}
-			if (_.includes(status, 'pending') && !_.includes(status, 'empty') && !_.includes(status, 'completed')) {
-				return 'pending';
-			}
-		}
-
-		if (showAllWeeks && weeks.count()) {
-			let allWeeks = {order: 0, termId: termId, status: getStatus(totalWeekStatus)}
+		if (termId === 'allTerms') {
+			let allWeeks = {order: 0, termId: termId}
 			self.added('weeks', 'allWeeks', allWeeks);
+		} else {
+			let groupId = Meteor.users.findOne({_id: this.userId}).info.groupId;
+			let weeks = Weeks.find({termId: termId, groupId: groupId, deletedOn: { $exists: false }}, {sort: {order: 1}, fields: {groupId: 0, userId: 0, createdOn: 0, deletedOn: 0}});
+			let totalWeekStatus = []
+
+			weeks.map((week) => {
+				week = weekStatus(week, week._id, studentId);
+				totalWeekStatus.push(week.status)
+				self.added('weeks', week._id, week);
+			});
+			
+			function getStatus(status) {
+				if (_.includes(status, 'partial')) {
+					return'partial';
+				}
+				if (_.includes(status, 'completed') && !_.includes(status, 'empty') && !_.includes(status, 'pending')) {
+					return 'completed';
+				}
+				if (_.includes(status, 'empty') && !_.includes(status, 'completed') && !_.includes(status, 'pending')) {
+					return 'empty';
+				}
+				if (_.includes(status, 'pending') && !_.includes(status, 'empty') && !_.includes(status, 'completed')) {
+					return 'pending';
+				}
+			}
+
+			if (showAllWeeks && weeks.count()) {
+				let allWeeks = {order: 0, termId: termId, status: getStatus(totalWeekStatus)}
+				self.added('weeks', 'allWeeks', allWeeks);
+			}
 		}
 
 		self.ready();
