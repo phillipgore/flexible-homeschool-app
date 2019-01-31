7e8f8c722b2e6f440548b4a9c932eaf2faf5b7d6
phillipgore <phillipagore@me.com>
2019-01-31 13:08:19 -0600

    html account emails

--- imports/startup/server/accounts.js
+++ imports/startup/server/accounts.js
@@ -21,31 +21,44 @@ Accounts.config({
   sendVerificationEmail: true,
 });
 
-Accounts.emailTemplates = {
-	from: 'Flexible Homeschool App <no-reply@aflexiblehomeschool.com>',
-	siteName: 'Flexible Homeschool App',
+Accounts.emailTemplates.siteName = 'Flexible Homeschool App';
+Accounts.emailTemplates.from = 'Flexible Homeschool App <no-reply@aflexiblehomeschool.com>';
 
-	resetPassword: {
-		subject: function(user, url) {
-			return 'Reset your password for ' + Accounts.emailTemplates.siteName + '.';
-		},
-		text: function(user, url) {
-			url = url.replace('#/reset-password/', 'reset/password/')
-			firstName = user.info.firstName
-			return firstName + ',\n\n Sorry you forgot your password. Click the link below (or copy and paste into your browser) and we’ll help you get a new one.\n\n\t' + url + '\n\nIf you didn\'t request a password reset, please ignore this email. \n\nThanks.';
-	    }
+Accounts.emailTemplates.resetPassword = {
+	subject() {
+		return 'Reset your password for ' + Accounts.emailTemplates.siteName + '.';
 	},
+	text(user, url) {
+		url = url.replace('#/reset-password/', 'reset/password/');
+		firstName = user.info.firstName;
 
-	verifyEmail: {
-		subject: function(user, url) {
-			return 'Please verify email address for ' + Accounts.emailTemplates.siteName + '.';
-		},
-		text: function(user, url) {
-			url = url.replace('#/verify-email/', 'verify/email/')
-			firstName = user.info.firstName
-			return firstName + ',\n\n Welcome to Flexible School Schedule. We need to verify your email address to complete your signup. Please click the verification link below (or copy and paste into your browser).\n\n\t' + url + '\n\nIf you have not signed up for Flexible School Schedule, please ignore this email. \n\nThanks.';
-		}
+		return firstName + ',\n\n Sorry you forgot your password. Click the link below (or copy and paste into your browser) and we’ll help you get a new one.\n\n\t' + url + '\n\nIf you didn\'t request a password reset, please ignore this email. \n\nThanks.';
+    },
+    html(user, url) {
+    	SSR.compileTemplate('resetPasswordEmail', Assets.getText('resetPasswordEmail.html'));
+
+		url = url.replace('#/reset-password/', 'reset/password/');
+		firstName = user.info.firstName;
+
+    	const html = SSR.render('resetPasswordEmail', { url, user });
+		return html;
+    }
+};
+
+Accounts.emailTemplates.verifyEmail = {
+	subject() {
+		return 'Please verify email address for ' + Accounts.emailTemplates.siteName + '.';
 	},
+	text(user, url) {
+		url = url.replace('#/verify-email/', 'verify/email/')
+		firstName = user.info.firstName
+		return firstName + ',\n\n Welcome to Flexible School Schedule. We need to verify your email address to complete your signup. Please click the verification link below (or copy and paste into your browser).\n\n\t' + url + '\n\nIf you have not signed up for Flexible School Schedule, please ignore this email. \n\nThanks.';
+	},
+    html(user, url) {
+    	SSR.compileTemplate('verifyEmail', Assets.getText('verifyEmail.html'));
+    	const html = SSR.render('verifyEmail', { url, user });
+		return html;
+    }
 };
 
 Accounts.onCreateUser((options, user) => {
