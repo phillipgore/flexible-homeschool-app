332adaca43774866b1625348ef88976ebf404588
phillipgore <phillipagore@me.com>
2019-04-11 14:27:51 -0500

    google analytics

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -4,5 +4,8 @@ BrowserPolicy.content.allowFontOrigin('data:');
 BrowserPolicy.content.allowOriginForAll('*.stripe.com',);
 BrowserPolicy.content.allowOriginForAll('*.youtube.com',);
 BrowserPolicy.content.allowOriginForAll('*.ytimg.com',);
+BrowserPolicy.content.allowOriginForAll('*.google-analytics.com',);
+
+
 
 
