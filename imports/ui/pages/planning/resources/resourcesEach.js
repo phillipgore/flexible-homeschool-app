e780ae3b2470d45fc2a85901ed26a1db48850411
phillipgore <phillipagore@me.com>
2019-06-13 17:06:28 -0500

    auto scrolling

--- imports/ui/pages/planning/resources/resourcesEach.js
+++ imports/ui/pages/planning/resources/resourcesEach.js
@@ -2,14 +2,28 @@ import {Template} from 'meteor/templating';
 import { Resources } from '../../../../api/resources/resources.js';
 import './resourcesEach.html';
 
+import _ from 'lodash'
+
+Template.resourcesList.onCreated( function() {
+	Session.set('unScrolled', true);
+});
+
 Template.resourcesEach.onRendered( function() {
-	let newScrollTop = document.getElementById(FlowRouter.getParam('selectedResourceId')).getBoundingClientRect().top - 180;
-	if (window.screen.availWidth > 640) {
-		document.getElementsByClassName('frame-two')[0].scrollTop = newScrollTop;
-	}
+	
 });
 
 Template.resourcesEach.helpers({
+	scroll: function() {
+		if (Session.get('unScrolled') && Template.instance().subscriptionsReady && Resources.find({_id: FlowRouter.getParam('selectedResourceId')}).count()) {
+			let newScrollTop = document.getElementById(FlowRouter.getParam('selectedResourceId')).getBoundingClientRect().top - 180;
+			if (window.screen.availWidth > 640) {
+				document.getElementsByClassName('frame-two')[0].scrollTop = newScrollTop;
+			}
+			Session.set('unScrolled', false);
+			return false;
+		}
+	},
+
 	selectedResourceType: function() {
 		return FlowRouter.getParam('selectedResourceType');
 	},
