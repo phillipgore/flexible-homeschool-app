5e5b0532325afacb5cf656ff5653ee1b2721a1de
phillipgore <phillipagore@me.com>
2019-06-21 12:17:46 -0500

    tag users with status in mailchimp

--- imports/startup/server/mailchimp.js
+++ imports/startup/server/mailchimp.js
@@ -2,6 +2,7 @@ import {Groups} from '../../api/groups/groups.js';
 
 import Mailchimp from 'mailchimp-api-v3';
 import md5 from 'md5';
+import _ from 'lodash';
 
 const mailchimp = new Mailchimp(Meteor.settings.private.mailchimp);
 
@@ -33,19 +34,29 @@ Meteor.methods({
 	},
 
 	mcTags: function(groupId) {
-		let tag = Groups.findOne({_id: groupId}).subscriptionStatus;
+		let tagProperties = [
+			{"name": "pending", "status": "inactive"},
+			{"name": "active", "status": "inactive"},
+			{"name": "freeTrial", "status": "inactive"},
+			{"name": "pausePending", "status": "inactive"},
+			{"name": "paused", "status": "inactive"},
+			{"name": "freeTrialExpired", "status": "inactive"},
+			{"name": "error", "status": "inactive"},
+		];
+		let tagName = Groups.findOne({_id: groupId}).subscriptionStatus;
+		let tagUpdateIndex = tagProperties.findIndex((tag => tag.name === tagName));
+		console.log(tagUpdateIndex)
+		tagProperties[tagUpdateIndex].status = "active";
 
 		Meteor.users.find({'info.groupId': groupId}).forEach(user => {
 			let email = user.emails[0].address;
 			let emailHash = md5(email);
 
-			console.log('tag: ' + user.info.firstName +' '+ user.info.lastName +': '+ email +': '+ tag)
+			console.log('user: ' + user.info.firstName +' '+ user.info.lastName +': '+ email)
+			console.log(tagProperties)
 
 			mailchimp.post('/lists/' + Meteor.settings.private.mailchimpListId + '/members/' + emailHash + '/tags', {
-				"tags": [{
-					"name": tag,
-					"status": "active",
-				}]
+				"tags": tagProperties
 			}).catch(function (error) {
 				console.log(error);
 			})
