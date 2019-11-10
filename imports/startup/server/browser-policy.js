8763748281c7d95acdc6e5ace1ac782aeac89984
phillipgore <phillipagore@me.com>
2019-11-10 15:56:44 -0600

    more flexible google analytics implimentation

--- imports/startup/server/browser-policy.js
+++ imports/startup/server/browser-policy.js
@@ -5,6 +5,7 @@ BrowserPolicy.content.allowOriginForAll('*.stripe.com',);
 BrowserPolicy.content.allowOriginForAll('*.youtube.com',);
 BrowserPolicy.content.allowOriginForAll('*.ytimg.com',);
 BrowserPolicy.content.allowOriginForAll('*.google-analytics.com',);
+BrowserPolicy.content.allowOriginForAll('*.googletagmanager.com',);
 
 
 
