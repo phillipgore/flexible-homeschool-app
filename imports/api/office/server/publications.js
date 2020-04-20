04bcb5027f4631613e3d99f940dd605ffed72355
phillipgore <phillipagore@me.com>
2020-04-20 10:07:29 -0500

    hotfix

--- imports/api/office/server/publications.js
+++ imports/api/office/server/publications.js
@@ -48,7 +48,7 @@ Meteor.publish('allAccountTotals', function(groupId) {
 	let activeGroupIds = groups.filter(group => group.appAdmin === false).map(group => (group._id));
 	let onlineGroupsIds = _.uniq(Meteor.users.find({'info.groupId': {$in: activeGroupIds}, 'presence.status': 'online'}).map(user => user.info.groupId))
 
-	Counts.publish(this, 'allAccountsCount', Groups.find());
+	Counts.publish(this, 'allAccountsCount', Groups.find({appAdmin: false}));
 	Counts.publish(this, 'onlineAccountsCount', Groups.find({_id: {$in: onlineGroupsIds}}));
 	Counts.publish(this, 'activeAccountsCount', Groups.find({appAdmin: false, subscriptionStatus: 'active'}));
 	Counts.publish(this, 'pausePendingAccountsCount', Groups.find({appAdmin: false, subscriptionStatus: 'pausePending'}));
